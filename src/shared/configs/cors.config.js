0fa56c0d5116643d68a4f29c1cc80d4e1024106a
Aasim-Qureshi <aasimq194@gmail.com>
2025-08-28 20:06:10 +0530

    setting deployment checks

--- src/shared/configs/cors.config.js
+++ src/shared/configs/cors.config.js
@@ -1,7 +1,5 @@
 const corsOptions = {
   origin: ['https://front-ui-five.vercel.app', 'http://localhost:5173'],
-  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization'],
   credentials: true,
 };
 
