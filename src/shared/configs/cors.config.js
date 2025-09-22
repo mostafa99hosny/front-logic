aeaca0ae4942d134a5619323bdd49cfaa4aafa67
Aasim-Qureshi <aasimq194@gmail.com>
2025-09-22 20:38:22 +0530

    Fixing Deployment

--- src/shared/configs/cors.config.js
+++ src/shared/configs/cors.config.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ['https://front-ui-five.vercel.app', 'http://localhost:5173'],
+  origin: ['https://front-ui-five.vercel.app', 'http://localhost:5173', 'http://64.227.186.182', 'http://64.227.186.182:5173'],
   credentials: true,
 };
 
